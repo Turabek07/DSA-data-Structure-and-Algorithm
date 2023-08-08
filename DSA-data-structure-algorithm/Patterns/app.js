3424584a5f90b758aa01c828847e33fd8cf07640
Turabek <turabekyusubov587@gmail.com>
2023-08-09 03:20:27 +0500

    patterns

---
+++ DSA-data-structure-algorithm/Patterns/app.js
@@ -0,0 +1,21 @@
+// PATTERN 1
+function pattern1(a){
+  for(let i = 0;i<a;i++){
+    let row = ''
+  for(let j = 0; j<a;j++){
+    row += '*'
+  }
+ console.log(row)
+  }
+}
+pattern1(5)
+
+// Answer will be 
+//    "*****"
+//    "*****"
+//    "*****"
+//    "*****"
+//    "*****"
+
+// --------------------------------------------
+
